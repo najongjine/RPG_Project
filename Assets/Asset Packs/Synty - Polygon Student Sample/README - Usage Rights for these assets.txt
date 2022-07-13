c0b3431df8a69296404562f6c8f12f548820ea63
najongjine <54340913+najongjine@users.noreply.github.com>
2022-07-13 13:01:28 +0900

    .

---
+++ Assets/Asset Packs/Synty - Polygon Student Sample/README - Usage Rights for these assets.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7d57e07d55195a0e14b3c00744af78310685b6a87c3ce9940295806b14c2226
+size 559
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8a0bc42a2957a1301a1e9a9bc5bd1915733afada26c565a430ad3ed6a9c53bc6
+size 832
